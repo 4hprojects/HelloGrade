056034860a5d3d611423bff188b4459d9c8d4057
4hprojects <hensonsagorsor@gmail.com>
2024-11-08 08:22:04 +0800

    Delete public directory

--- public/js/validation.js
+++
@@ -1,8 +0,0 @@
-// validation.js
-
-// Function to validate password strength
-function isValidPassword(password) {
-    // Requires at least one uppercase letter, one lowercase letter, one number, and at least 8 characters
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    return passwordRegex.test(password);
-}
