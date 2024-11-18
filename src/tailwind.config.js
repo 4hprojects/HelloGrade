b54a5586b38516f2eb542f464f1ababe4f599c10
4hprojects <hensonsagorsor@gmail.com>
2024-11-18 14:47:22 +0800

    Add files via upload

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: ['attribute', 'data-theme="dark"'],
+    content: [
+      './public/**/*.{html,js}',
+      // Add other paths as needed
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
