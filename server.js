959cce3ee057863ea62bd7091b3313fbbff45240
4hprojects <hensonsagorsor@gmail.com>
2025-01-27 14:58:54 +0800

    Update server.js

--- server.js
+++ server.js
@@ -89,6 +89,21 @@ app.get('/api/config', (req, res) => {
     });
 });
 
+const serviceAccount = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
+};
+
+
 //-----------------------------------------------------------------
 // BYTe Fun Run 2025: Sign-Up POST Route
 //-----------------------------------------------------------------
@@ -120,9 +135,9 @@ app.post('/api/bytefunrun2025', async (req, res) => {
         const path = require('path');
         // ...
         const auth = new google.auth.GoogleAuth({
-          keyFile: path.join(__dirname, 'credentials', 'service-account.json'),
-          scopes: ['https://www.googleapis.com/auth/spreadsheets']
-        });
+            credentials: serviceAccount,
+            scopes: ['https://www.googleapis.com/auth/spreadsheets']
+          });
         
         const authClient = await auth.getClient();
         const sheets = google.sheets({ version: 'v4', auth: authClient });
